d444948eed75e8e05e18a48626aa30948e628fd9
Jhonny <jhonnyvanckruz@gmail.com>
2018-03-07 22:16:28 -0400

    cambio rápido en carrito

--- src/pages/cart/cart.ts
+++ src/pages/cart/cart.ts
@@ -10,25 +10,26 @@ import { Storage } from '@ionic/storage';
 export class CartPage {
   items: any;
   total: any;
-  
+  dataForPay: any;
+
   constructor(public navCtrl: NavController, public navParams: NavParams, public storage: Storage) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad CartPage');
+    this.getCarrito();
+  }
 
+  getCarrito(){
     this.storage.get("carrito").then((carrito) => {
       this.items = JSON.parse(carrito);
-      // this.total = this.items.reduce( (valorAnterior, valorActual, indice, vector) => {
-      //   console.log(typeof valorAnterior.precio, typeof valorActual.precio)
-      //   console.log(valorAnterior.precio, valorActual.precio)
-      //   return parseFloat(valorAnterior.precio) + parseFloat(valorActual.precio);
-      // })
       this.total = 0;
-      for (var i in this.items) { this.total += parseFloat(this.items[i].precio) * parseFloat(this.items[i].cantidad); }
-      console.log(this.total)
-      console.log(this.items)
-    });    
+      // for (var i in this.items) { this.total += parseFloat(this.items[i].precio) * parseFloat(this.items[i].cantidad); }
+      for (let i of this.items) { 
+        this.total += parseFloat(i.precio) * parseFloat(i.cantidad); 
+      }
+      
+    });     
   }
 
   ProccessPayment(){
